4d1ea5e9e7bc6af0c6c99f751d9df6d6562a40d1
JavohirJalilov <javohirjaliloff@gmail.com>
2023-02-21 20:06:10 +0500

    nested button

--- bot.py
+++ bot.py
@@ -19,22 +19,41 @@ def start(update: Update, context: CallbackContext):
    chat_id = update.message.chat.id
    first_name = update.message.chat.first_name
 
-   button1 = KeyboardButton(text='DOG')
-   button2 = KeyboardButton(text="CAT")
+   button1 = KeyboardButton(text='Contact')
+   button2 = KeyboardButton(text="Category")
 
    reply_markup = ReplyKeyboardMarkup([[button1, button2]], resize_keyboard=True)
 
    bot.sendMessage(chat_id, f'Hello, {first_name}', reply_markup=reply_markup)
 
-def echo(update: Update, context:CallbackContext):
+def contact(update, context):
+   bot = context.bot
+   chat_id = update.message.chat.id
+
+   bot.sendMessage(chat_id, "+998992344334")
+
+def category(update, context):
+   bot = context.bot
+   chat_id = update.message.chat.id
+
+   button1 = KeyboardButton(text="G'ijduvon")
+   button2 = KeyboardButton(text="Kogon")
+
+   reply_markup = ReplyKeyboardMarkup([[button1, button2]], resize_keyboard=True)
+   bot.sendMessage(chat_id, f'Qayerga ketasiz?', reply_markup=reply_markup)
+
+def echo(update, context):
    bot = context.bot
    chat_id = update.message.chat.id
    text = update.message.text
+
    bot.sendMessage(chat_id, text)
 
 
 updater = Updater(token=TOKEN)
 updater.dispatcher.add_handler(CommandHandler("start", start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text("Contact"), contact))
+updater.dispatcher.add_handler(MessageHandler(Filters.text("Category"), category))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
 updater.start_polling()
